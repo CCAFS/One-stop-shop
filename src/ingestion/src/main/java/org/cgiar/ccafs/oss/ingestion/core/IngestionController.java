30bee20745a3eb8e697f0dd0f7f30937a2001367
Eduardo Quiros-Campos <edoquiros@gmail.com>
2016-12-05 21:50:28 -0600

    WIP on the ingestion framework

--- src/ingestion/src/main/java/org/cgiar/ccafs/oss/ingestion/core/IngestionController.java
+++ src/ingestion/src/main/java/org/cgiar/ccafs/oss/ingestion/core/IngestionController.java
@@ -2,11 +2,14 @@ package org.cgiar.ccafs.oss.ingestion.core;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.cgiar.ccafs.oss.ingestion.connectors.ConnectorManager;
 
 import java.io.File;
 
 public class IngestionController {
   private static final Logger logger = LogManager.getLogger(IngestionController.class);
+  private ConnectorManager connectorManager;
+
   public IngestionController(String configurationDirectory) {
     File configFile = new File(configurationDirectory, "config.json");
     if (!configFile.exists()) {
