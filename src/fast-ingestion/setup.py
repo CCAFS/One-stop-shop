0cf8589dcde924773f8ef6cb0b7bfaecf8525638
Eduardo Quiros-Campos <edoquiros@gmail.com>
2016-12-16 00:56:29 -0600

    Python Fast Crawler changes

---
+++ src/fast-ingestion/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='fast-ingestion',
+    version='1.0',
+    packages=['ingestion'],
+    install_requires=['requests'],
+    url='',
+    license='',
+    author='Eduardo Quiros-Campos',
+    author_email='',
+    description=''
+)
